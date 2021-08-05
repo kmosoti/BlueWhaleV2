afacf0666f0e872191aee277083b2259bc0059d3
EC2 Default User <ec2-user@ip-172-31-88-80.ec2.internal>
2021-08-05 19:55:34 +0000

    Added More Columns

--- src/Tablecustom.js
+++ src/Tablecustom.js
@@ -8,6 +8,10 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import CircularProgress from '@material-ui/core/CircularProgress';
+
+import Select from '@material-ui/core/Select';
+import MenuItem from "@material-ui/core/MenuItem";
+
 import './App.css';
 
 import {withStyles, theme } from '@material-ui/core/styles';
@@ -26,6 +30,9 @@ import {withStyles, theme } from '@material-ui/core/styles';
       TableHeader: {
         fontWeight: "fontWeightBold",
         backgroundColor: "grey"
+      },
+      Select: {
+          width: "maxWidth"
       }
     })
 class TableCustom extends React.Component{
@@ -66,7 +73,7 @@ class TableCustom extends React.Component{
         console.log("Getting Price Data: ")
         console.log(RESPONSE_DATA)
         priceData.name = RESPONSE_DATA[0].name
-        priceData.price = RESPONSE_DATA[0]["price per hour USD"]
+        priceData.price = RESPONSE_DATA[0]["price per hour USD"]*750.001
         
         priceObjArray.push(priceData)
         this.setState({priceCache: priceData})
@@ -98,33 +105,43 @@ class TableCustom extends React.Component{
             2:"Resource Type",
             3:"Virtual Machine Type",
             4:"AWS Matches",
-            5:"Price(Monthly)"
+            5:"Pricing (Monthly)",
+            6:"vCPUs",
+            7:"Memory (GB)"
         }
         return(
             <TableRow>
                 <TableCell className="tableHeadFont" key={tableHeaders[0]}>{tableHeaders[0]}</TableCell>
-                <TableCell className="tableHeadFont" key={tableHeaders[1]}>{tableHeaders[1]}</TableCell>
-                <TableCell className="tableHeadFont" key={tableHeaders[2]}>{tableHeaders[2]}</TableCell>
-                <TableCell className="tableHeadFont" key={tableHeaders[4]}>{tableHeaders[4]}</TableCell>
-                <TableCell className="tableHeadFont" key={tableHeaders[5]}>{tableHeaders[5]}</TableCell>
+                <TableCell className="tableHeadFont" align="right" key={tableHeaders[1]}>{tableHeaders[1]}</TableCell>
+                <TableCell className="tableHeadFont" align="right" key={tableHeaders[2]}>{tableHeaders[2]}</TableCell>
+                <TableCell className="tableHeadFont" align="right" key={tableHeaders[3]}>{tableHeaders[3]}</TableCell>
+                <TableCell className="tableHeadFont" align="right" key={tableHeaders[6]}>{tableHeaders[6]}</TableCell>
+                <TableCell className="tableHeadFont" align="right" key={tableHeaders[7]}>{tableHeaders[7]}</TableCell>
+                <TableCell className="tableHeadFont" align="right" key={tableHeaders[4]}>{tableHeaders[4]}</TableCell>
+                <TableCell className="tableHeadFont" align="right" key={tableHeaders[5]}>AWS {tableHeaders[5]}</TableCell>
             </TableRow>
         );
+        //                
     }
     
     renderTableRows = () =>{
         //console.log("Getting Rows: "+JSON.stringify(this.state.dataStore))
         //this.getMatchedPrice(this.state.dataStore[0].name)
         return this.state.dataStore.map(data => {
-            let i = 0;
+            console.log(data)
             return (
-                <TableRow key={i++}>
+                <TableRow key={data.name}>
                     <TableCell key={data.provider}>{data.provider}</TableCell>
                     <TableCell align="right">{data.name}</TableCell>
                     <TableCell align="right">{data["resource type"]}</TableCell>
-                    <TableCell align="right">{data.AWS_matches[0]}</TableCell>
-                    <TableCell key="price" align="right">{this.state.priceCache.price}</TableCell>
+                    <TableCell align="right">{data["virtual machine type"]}</TableCell>
+                    <TableCell align="right">{data.vCPUs}</TableCell>
+                    <TableCell align="right">{data.MemoryGB}</TableCell>
+                    <TableCell align="right">{JSON.stringify(data.AWS_matches)}</TableCell>   
+                    <TableCell className="tablePrice" key="price" align="right">${this.state.priceCache.price}</TableCell>
                 </TableRow>
             )
+//                    <TableCell align="right"><Select className={}><MenuItem>Test</MenuItem></Select></TableCell>             
         })
     }
     render(){
